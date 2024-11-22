112fa4ac2e55f0a4e50247182ebfbbb193365be1
Léo Vieira Martins <126921432+leovmartinss@users.noreply.github.com>
2024-11-21 23:45:15 -0300

    Add files via upload

---
+++ demo/src/test/java/com/simulado/prova/SimuladoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simulado.prova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
